f32059be7c9c2d76fe2479a9e9ed1adee1f56bc5
ilya07092000 <ischenko2012@gmail.com>
2023-12-11 08:28:46 +0200

    feat: binary tree inorder traversal

---
+++ binary-tree-inorder-traversal/README.md
@@ -0,0 +1,3 @@
+## Task Provider - Leetcode (easy)
+
+[Task Link](https://leetcode.com/problems/binary-tree-inorder-traversal/description/?envType=daily-question&envId=2023-12-09)
---
+++ binary-tree-inorder-traversal/index.ts
@@ -0,0 +1,34 @@
+* class TreeNode {
+  *     val: number
+  *     left: TreeNode | null
+  *     right: TreeNode | null
+  *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+  *         this.val = (val===undefined ? 0 : val)
+  *         this.left = (left===undefined ? null : left)
+  *         this.right = (right===undefined ? null : right)
+  *     }
+  * }
+  */
+ 
+ function inorderTraversal(root: TreeNode | null): number[] {
+     const result = [];
+     
+     const traverse = (node) => {
+        if (node) {
+            
+             if (node.left) {
+                 traverse(node.left)
+             }
+ 
+              result.push(node.val);
+ 
+              if (node.right) {
+                 traverse(node.right)
+             }
+ 
+        }
+     }
+ 
+     traverse(root);
+     return result;
+ };

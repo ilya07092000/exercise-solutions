078064bfd44f7f806b19a9f0bb5ac3fc1c006fbf
ilya07092000 <ischenko2012@gmail.com>
2024-03-16 11:39:28 +0200

    refactor: contiguous-array

--- js/contiguous-array/index.ts
+++ js/contiguous-array/index.ts
@@ -3,7 +3,7 @@ function findMaxLength(nums: number[]): number {
   /**
    * KEEP PREFIX SUM
    */
-  let sumArr = [0];
+  let counter = 0;
   /**
    * Save The Prefix Sum Index
    */
@@ -14,22 +14,21 @@ function findMaxLength(nums: number[]): number {
      * For 0 we descrease and for 1 increase  the prefix sub by 1
      */
     const value = nums[i] === 0 ? -1 : +1;
-    const sum = sumArr[i] + value;
-    sumArr.push(sum);
+    counter += value;
 
     /**
      * If sum is 0 it means that we met equal distribution of 1 and 0
      */
-    if (sum === 0) {
+    if (counter === 0) {
       result = i + 1;
-    } else if (sum in sumMap) {
+    } else if (counter in sumMap) {
       // if the prefix sum is already is in the sum map
       // then the difference index betwen this two sums is the conigious array length
-      const firstSumIdx = sumMap[sum];
+      const firstSumIdx = sumMap[counter];
       const arrLength = i - firstSumIdx;
       result = Math.max(arrLength, result);
     } else {
-      sumMap[sum] = i;
+      sumMap[counter] = i;
     }
   }
 

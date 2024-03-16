b03a7d6354b44af348771b2861119b959bba6191
ilya07092000 <ischenko2012@gmail.com>
2024-03-16 11:34:29 +0200

    feat: contiguous-array

---
+++ js/contiguous-array/README.md
@@ -0,0 +1,3 @@
+## Task Provider - Leetcode (medium)
+
+[Task Link](https://leetcode.com/problems/contiguous-array/description/?envType=daily-question&envId=2024-03-16)
---
+++ js/contiguous-array/index.ts
@@ -0,0 +1,37 @@
+function findMaxLength(nums: number[]): number {
+  let result = 0;
+  /**
+   * KEEP PREFIX SUM
+   */
+  let sumArr = [0];
+  /**
+   * Save The Prefix Sum Index
+   */
+  const sumMap = {};
+
+  for (let i = 0; i < nums.length; i += 1) {
+    /**
+     * For 0 we descrease and for 1 increase  the prefix sub by 1
+     */
+    const value = nums[i] === 0 ? -1 : +1;
+    const sum = sumArr[i] + value;
+    sumArr.push(sum);
+
+    /**
+     * If sum is 0 it means that we met equal distribution of 1 and 0
+     */
+    if (sum === 0) {
+      result = i + 1;
+    } else if (sum in sumMap) {
+      // if the prefix sum is already is in the sum map
+      // then the difference index betwen this two sums is the conigious array length
+      const firstSumIdx = sumMap[sum];
+      const arrLength = i - firstSumIdx;
+      result = Math.max(arrLength, result);
+    } else {
+      sumMap[sum] = i;
+    }
+  }
+
+  return result;
+}
